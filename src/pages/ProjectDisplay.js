1cf8a039ec9d1727c93f64d17ce42267f8bfc39d
Nishant Pansuriya <nishantp1337@gmail.com>
2023-08-12 22:30:39 -0400

    Fixed build issue

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList";
-import GitHubIcon from "@material-ui/icons/GitHub";
 import "../styles/ProjectDisplay.css";
 
 function ProjectDisplay() {
